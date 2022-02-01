dec95b98d6666225c1da32c244baac8db0411c6d
tanvirtonmoy42 <48643228+tanvirtonmoy42@users.noreply.github.com>
2022-02-01 22:27:20 +0600

    first commit

---
+++ flutter_getx/lib/detail_controller.dart
@@ -0,0 +1,14 @@
+import 'package:get/get.dart';
+
+class DetailController extends GetxController {
+  var fav = 0.obs;
+
+  void favCounter() {
+    if (fav.value == 1) {
+      Get.snackbar('Loved it', "You Already Loved it");
+    } else {
+      fav.value++;
+      Get.snackbar("Loved it", "You just Loved it");
+    }
+  }
+}
